034b1cc13cdbae2812055d257bd39cde27c268d8
David DuBois <david.dubois@techfield.us>
2018-10-31 12:02:54 -0400

    1.1

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,8 +1,25 @@
 object HelloWorld {
   def square(x: Int): Int = x*x
 
+  val list = List(1, 2, 3)
+
+  def prList(): Unit = {
+    list.foreach(x => println(x))
+    list.foreach(println)
+
+    list.map(x => x + 2)
+    println(list.map(_ + 2))
+
+    list.filter(x => x % 2 == 1)
+    println(list.filter(_ % 2 == 1))
+
+    list.reduce((x, y) => x + y)
+    println(list.reduce(_ + _))
+  }
+
   def main(args: Array[String]){
     println(square(10))
+    prList()
     println("HW")
   }
 }
